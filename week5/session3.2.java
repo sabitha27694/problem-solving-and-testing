9ec552a20cf53849416cc96603b89e41af92c13f
sabitha27694 <vtu27694@veltech.edu.in>
2026-08-24 09:39:48 +0530

    Add files via upload

---
+++ week5/session3.2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int alternatingCharacters(String s) {
+
+        int count = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
