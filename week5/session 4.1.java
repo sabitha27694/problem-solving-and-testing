50e86d5e805178e5d004dcb934b389a638917e2a
sabitha27694 <vtu27694@veltech.edu.in>
2026-08-24 09:42:59 +0530

    Add files via upload

---
+++ week5/session 4.1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            HashSet<Character> set = new HashSet<>();
+
+            for (int j = i; j < s.length(); j++) {
+
+                if (set.contains(s.charAt(j))) {
+                    break;
+                }
+
+                set.add(s.charAt(j));
+
+                maxLen = Math.max(maxLen, j - i + 1);
+            }
+        }
+
+        return maxLen;
+    }
+}
