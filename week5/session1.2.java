e4b488cea6620a0947b2d4b640dccc91e6ddf523
sabitha27694 <vtu27694@veltech.edu.in>
2026-08-24 09:30:40 +0530

    Add files via upload

---
+++ week5/session1.2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+
+                int sum = 0;
+
+                for (int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+
+        return maxSum;
+    }
+}
